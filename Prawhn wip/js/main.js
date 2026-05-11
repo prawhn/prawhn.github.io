310440bbed1b345b8eae0631e825fec4888772a8
prawhn <prawntheory@gmail.com>
2026-05-11 18:37:08 +0100

    Delete Prawhn wip directory

--- Prawhn wip/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.service-card').forEach(card => {
-	const inner = card.querySelector('.card-inner');
-	if (!inner) return;
-
-	card.addEventListener('mouseenter', () => inner.classList.add('is-flipped'));
-	card.addEventListener('mouseleave', () => inner.classList.remove('is-flipped'));
-});
